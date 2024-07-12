d919e0ef985b6e232991309641614a82f9a897be
kellynvd <kellynvd@gmail.com>
2024-07-12 08:08:35 -0300

    Add branding colors and fonts

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -8,10 +8,19 @@ module.exports = {
     './app/views/**/*',
   ],
   theme: {
+    colors:{
+      sunset: '#F65B4E',
+      twilight: '#29319F',
+      morning: '#FFBA7C',
+      fog: '#FFDEEF',
+      eclipse: '#573353',
+    },
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        'manrope': ["Manrope", 'sans-serif'],
+        'klasik': ["Klasik", 'sans-serif'],
       },
+
     },
   },
   plugins: [
